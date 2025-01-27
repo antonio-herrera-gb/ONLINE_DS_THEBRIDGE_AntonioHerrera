53fb6f5dd9f0ffe8d09fa9d75e3c243c6bd117c7
antonio-herrera-gb <antonioherrera764@gmail.com>
2025-01-27 17:14:45 +0100

    s12 u1

--- 04_Machine_Learning/Sprint_12/Unidad_01/Practica_Obligatoria/bootcampviztools.py
+++ 04_Machine_Learning/Sprint_12/Unidad_01/Practica_Obligatoria/bootcampviztools.py
@@ -273,4 +273,32 @@ def bubble_plot(df, col_x, col_y, col_size, scale = 1000):
     plt.title(f'Burbujas de {col_x} vs {col_y} con Tamaño basado en {col_size}')
     plt.show()
 
-
+def cardinalidad(df, umbral_categoria, umbral_continua):
+    resultados = []
+    
+    for columna in df.columns:
+        # Calcular la cardinalidad como el número de valores únicos
+        valores_unicos = df[columna].nunique()
+        # Calcular el porcentaje de cardinalidad
+        porcentaje_cardinalidad = valores_unicos / len(df) * 100
+        
+        # Clasificación según la cardinalidad
+        if valores_unicos == 2:
+            tipo = "Binaria"
+        elif valores_unicos < umbral_categoria:
+            tipo = "Categórica"
+        else:
+            if porcentaje_cardinalidad >= umbral_continua:
+                tipo = "Numérica Continua"
+            else:
+                tipo = "Numérica Discreta"
+        
+        resultados.append({
+            "Columna": columna,
+            "Cardinalidad": valores_unicos,
+            "Porcentaje Cardinalidad": porcentaje_cardinalidad,
+            "Tipo": tipo
+        })
+    
+    # Convertir los resultados a un DataFrame
+    return pd.DataFrame(resultados)
